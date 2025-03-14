91116ec2c30616f0d0545761cfe41942579e7ed4
Madhav Gandhasiri <madhavgandhasiri@gmail.com>
2025-03-14 20:27:49 +0530

    all changes made

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from "../Button/Button";
-import Logo from "../Logo/Logo";
 import Search from "../Search/Search";
 import styles from "./Navbar.module.css";
+import LogoImage from "../../assets/Logo.png";
 
-function Navbar({ searchData }) {
+export default function Navbar() {
   return (
-    <nav className={styles.navbar}>
-      <Link to="/">
-        <Logo />
-      </Link>
-      <Search
-        placeholder="Search a song of your choice"
-        searchData={searchData}
-      />
-      <Button>Give Feedback</Button>
-    </nav>
+    <>
+      <nav className={styles.navbar}>
+        <div className={styles.logoDiv}>
+          <Link to="/">
+            <img src={LogoImage} alt="logo" width={67} />
+          </Link>
+        </div>
+        <Search search={"Search a song of your choice"} />
+        <Button children="Give Feedback" />
+      </nav>
+    </>
   );
 }
-
-export default Navbar;
--- qtify/src/helpers/helpers.jsx
+++
@@ -1,5 +0,0 @@
-export function truncate(str, num) {
-    if (str.length <= num) return str;
-    return str.slice(0, num) + "...";
-  }
-  
