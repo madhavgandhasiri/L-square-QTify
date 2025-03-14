097a58813069c2c05666bb2a353da8fda47745be
Madhav Gandhasiri <madhavgandhasiri@gmail.com>
2025-03-14 21:07:18 +0530

    bugs resolved for logo

--- qtify/src/components/Search/Search.js
+++ qtify/src/components/Search/Search.js
@@ -2,10 +2,10 @@ import React from "react";
 import {ReactComponent as SearchIcon} from "../../assets/search-icon.svg"
 import styles from "./Search.module.css"
 
-const Search = ({ search }) => {
+const Search = ({ search, placeholder }) => {
   return (
     <form className={styles.wrapper}>
-      <input className={styles.search} placeholder={search}></input>
+      <input className={styles.search} placeholder={placeholder}></input>
       <button className={styles.searchButton} type="submit">
         <SearchIcon />
       </button>
